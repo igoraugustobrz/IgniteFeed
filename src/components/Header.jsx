7244df44d6d91bb1ca57b06ac5981149ebef075a
Igor Augusto <igoraugustobz@gmail.com>
2023-11-09 07:34:10 -0300

    feat: adição dos arquivos iniciais

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from './Header.module.css';
+
+
+
+export const Header = () => {
+    return (
+        <>
+
+            <header className={styles.header}>
+                <strong>Ignite Feed</strong>
+            </header>
+        </>
+    )
+}
