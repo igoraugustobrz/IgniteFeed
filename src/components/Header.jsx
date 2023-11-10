80d58171d0f37f66f9935f60d665f512ed35e3ad
Igor Augusto <igoraugustobz@gmail.com>
2023-11-10 11:45:35 -0300

    style: componente Header estilizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import styles from './Header.module.css';
-
-
+import igniteLogo from '../assets/Ignite-logo.svg';
 
 export const Header = () => {
     return (
         <>
 
             <header className={styles.header}>
-                <strong>Ignite Feed</strong>
+                <img
+                    src={igniteLogo}
+                    alt='Logotipo do Ignite'
+                />
             </header>
         </>
     )
